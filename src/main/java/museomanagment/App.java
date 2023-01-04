9bc244843f28bfd3659aaa2633d59635a83ecec5
Domenico Francesco Giacobbi <domenico.giacobbi@studio.unibo.it>
2023-01-04 23:12:14 +0100

    init project

---
+++ src/main/java/museomanagment/App.java
@@ -0,0 +1,21 @@
+package museomanagment;
+
+/**
+ * Main class.
+ */
+public final class App {
+
+
+    private App() {
+
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(final String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
