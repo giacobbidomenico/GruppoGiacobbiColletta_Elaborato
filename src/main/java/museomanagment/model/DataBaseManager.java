939d6d64d778f55a5e48c992715a176138062bf9
Lorenzo Colletta <lorenzo.colletta@studio.unibo.it>
2023-01-05 21:54:19 +0100

    feat: add DataBaseManagement interface

---
+++ src/main/java/museomanagment/model/DataBaseManager.java
@@ -0,0 +1,35 @@
+package museomanagment.model;
+
+import java.sql.ResultSet;
+
+/**
+ * Models a basic databaseManager.
+ */
+public interface DataBaseManager {
+
+
+    /**
+     * Closes connection.
+     */
+    void close();
+
+    /**
+     * set the query to be executed.
+     * @param query
+     */
+    void setQuery(String query);
+
+    /**
+     * 
+     * @param parameter
+     * @return true if parameter was set
+     */
+    boolean addParameter(String parameter);
+
+    /**
+     * Executes query.
+     * @return result from the query
+     */
+    ResultSet executeQuery();
+
+}
