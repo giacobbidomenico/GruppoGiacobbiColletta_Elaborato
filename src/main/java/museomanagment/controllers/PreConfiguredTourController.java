e14b1d9edddd4c4d54f65de7547060c922a54df6
Domenico Francesco Giacobbi <domenico.giacobbi@studio.unibo.it>
2023-01-07 11:19:54 +0100

    fear: insert model in the PreConfiguredTourController class

--- src/main/java/museomanagment/controllers/PreConfiguredTourController.java
+++ src/main/java/museomanagment/controllers/PreConfiguredTourController.java
@@ -1,13 +1,22 @@
 package museomanagment.controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
+import museomanagment.model.MuseoManagement;
+import museomanagment.model.MuseoManager;
 
 /**
  * Controller for inserting a new pre-configured tour.
  */
 public class PreConfiguredTourController {
+    private final MuseoManagement museoManagment = new MuseoManager();
+    private final Alert alertError = new Alert(AlertType.ERROR, 
+                                               "Errore nell'inserimento di un tour pre configurato");
+    private final Alert alertSuccess = new Alert(AlertType.CONFIRMATION, 
+                                                 "Tour pre configurato inserito con successo");
     @FXML
     private TextField numberSeats;
     @FXML
@@ -20,7 +29,7 @@ public class PreConfiguredTourController {
      * museum areas from the database and inserting them in the related table.
      */
     public void setArea() {
-        //TODO
+        this.area.getItems().setAll(this.museoManagment.getAreas());
     }
 
     /**
@@ -28,6 +37,17 @@ public class PreConfiguredTourController {
      * fetching the data from the application.
      */
     public void insertPreConfiguredTour() {
-        //TODO
+        if (numberSeats.getText() == null
+                || numberSeats.getText().isBlank()
+                || price.getText() == null
+                || price.getText().isBlank()
+                || this.area.getItems().isEmpty()) {
+            alertError.show();
+            return;
+        }
+        this.museoManagment.tourStandardRegistration(numberSeats.getText().trim(), 
+                                                     price.getText().trim(),
+                                                     this.area.getValue());
+        alertSuccess.show();
     }
 }
