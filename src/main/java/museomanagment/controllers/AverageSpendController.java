3c6cd1de487bacba8a073fcdefa298a1a7545a54
Lorenzo Colletta <lorenzo.colletta@studio.unibo.it>
2023-01-07 10:21:08 +0100

    feat: implement AverageSpendController

--- src/main/java/museomanagment/controllers/AverageSpendController.java
+++ src/main/java/museomanagment/controllers/AverageSpendController.java
@@ -3,16 +3,92 @@ package museomanagment.controllers;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import museomanagment.controllers.TicketsController.Ticket;
+import museomanagment.model.MuseoManagement;
+import museomanagment.model.MuseoManager;
 
 /**
  * Controller related to the calculation of the average spend made by clients.
  */
 public class AverageSpendController implements Initializable {
+    private final MuseoManagement museoManagment = new MuseoManager();
+
     @FXML
-    private TableView<String> averageSpend;
+    private TableView<AvgRow> average;
+    @FXML
+    private TableColumn<AvgRow, String> email;
+    @FXML
+    private TableColumn<AvgRow, String> pw;
+    @FXML
+    private TableColumn<AvgRow, String> nome;
+    @FXML
+    private TableColumn<AvgRow, String> cognome;
+    @FXML
+    private TableColumn<AvgRow, String> avg;
+    @FXML
+    private TableColumn<AvgRow, String> code;
+
+    /**
+     * 
+     */
+    public class AvgRow {
+        private final String email;
+        private final String pw;
+        private final String nome;
+        private final String cognome;
+        private final String avg;
+        private final String code;
+
+        /**
+         * @param email
+         * @param pw
+         * @param nome
+         * @param cognome
+         * @param avg
+         * @param code
+         */
+        public AvgRow(final String code, final String email, final String pw, final String nome, final String cognome, final String avg) {
+            this.email = email;
+            this.pw = pw;
+            this.nome = nome;
+            this.cognome = cognome;
+            this.avg = avg;
+            this.code = code;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public String getPw() {
+            return pw;
+        }
+
+        public String getNome() {
+            return nome;
+        }
+
+        public String getCognome() {
+            return cognome;
+        }
+
+        public String getAvg() {
+            return avg;
+        }
+
+        public String getCode() {
+            return code;
+        }
+
+
+    }
 
     /**
      * {@inheritDoc}
@@ -27,7 +103,27 @@ public class AverageSpendController implements Initializable {
      * for each user and inserts the result in the relative table.
      */
     public void updateAverageSpend() {
-        //TODO
+        this.email.setCellValueFactory(new PropertyValueFactory<AvgRow, String>("email"));
+        code.setCellValueFactory(new PropertyValueFactory<AvgRow, String>("code"));
+        this.pw.setCellValueFactory(new PropertyValueFactory<AvgRow, String>("pw"));
+        this.nome.setCellValueFactory(new PropertyValueFactory<AvgRow, String>("nome"));
+        this.cognome.setCellValueFactory(new PropertyValueFactory<AvgRow, String>("cognome"));
+        this.avg.setCellValueFactory(new PropertyValueFactory<AvgRow, String>("avg"));
+
+
+        this.average.getItems().clear();
+        final var table = this.museoManagment.usersExpenseAvg();
+        table.forEach(e -> {
+            final AvgRow row = new AvgRow(e.get(0), 
+                                             e.get(1),
+                                             e.get(2),
+                                             e.get(3),
+                                             e.get(4),
+                                             e.get(5));
+            final ObservableList<AvgRow> list = this.average.getItems();
+            list.add(row);
+            this.average.setItems(list);
+        });
     }
 
 
