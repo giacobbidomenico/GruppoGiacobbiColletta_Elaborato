875e42dafc5d9c78f0f4823b459f7e936be0587b
Domenico Francesco Giacobbi <domenico.giacobbi@studio.unibo.it>
2023-01-07 09:13:14 +0100

    fear: insert model in the User Promotions Controller class

--- src/main/java/museomanagment/controllers/UserPromotionsController.java
+++ src/main/java/museomanagment/controllers/UserPromotionsController.java
@@ -1,13 +1,26 @@
 package museomanagment.controllers;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
+import museomanagment.model.MuseoManagement;
+import museomanagment.model.MuseoManager;
 
 /**
  * Controller related to the entry of a new user promotion.
  */
 public class UserPromotionsController {
+    private final MuseoManagement museoManagment = new MuseoManager();
+    private final Alert alertError = new Alert(AlertType.ERROR, 
+                                               "Errore nell'inserimento della promozione utente");
+    private final Alert alertSuccess = new Alert(AlertType.CONFIRMATION, 
+                                                 "Promozione utente inserita correttamente");
+
     @FXML
     private TextField namePromotion;
     @FXML
@@ -19,12 +32,20 @@ public class UserPromotionsController {
     @FXML
     private ComboBox<String> userType;
 
+    private List<String> getPeriods() {
+        return this.museoManagment.getPeriods()
+                                  .stream()
+                                  .map(e -> e.toString())
+                                  .collect(Collectors.toList());
+    }
+
     /**
      * Method that calls the query that takes care of picking up the
      * activity period from the database and inserting them in the related ComboBox.
      */
     public void setActivityPeriod() {
-        //TODO
+        this.activityPeriod.getItems().clear();
+        this.activityPeriod.getItems().addAll(this.getPeriods());
     }
 
     /**
@@ -32,7 +53,8 @@ public class UserPromotionsController {
      * validity period from the database and inserting them in the related ComboBox.
      */
     public void setValidityPeriod() {
-        //TODO
+        this.validityPeriod.getItems().clear();
+        this.validityPeriod.getItems().addAll(this.getPeriods());
     }
 
     /**
@@ -40,7 +62,8 @@ public class UserPromotionsController {
      * users from the database and inserting them in the related ComboBox.
      */
     public void setUserType() {
-        //TODO
+        this.userType.getItems().clear();
+        this.userType.getItems().addAll(this.museoManagment.getUserTypes());
     }
 
     /**
@@ -48,6 +71,20 @@ public class UserPromotionsController {
      * user promotion, inserting the data fetched from the application.
      */
     public void insertUserPromotion() {
-        //TODO
+        if (namePromotion.getText() == null
+                || namePromotion.getText().isBlank()
+                || discount.getText() == null
+                || activityPeriod.getItems().isEmpty()
+                || validityPeriod.getItems().isEmpty()
+                || userType.getItems().isEmpty()) {
+            alertError.show();
+            return;
+        }
+        this.museoManagment.userTypePromotionRegistration(namePromotion.getText().trim(), 
+                                                          discount.getText().trim(), 
+                                                          activityPeriod.getSelectionModel().getSelectedIndex(), 
+                                                          validityPeriod.getSelectionModel().getSelectedIndex(), 
+                                                          userType.getValue().trim());
+        alertSuccess.show();
     }
 }
